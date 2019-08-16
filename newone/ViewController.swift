ee062623add9a62fb490c4afcf06bdb39ef73ec7
Panacea015 <panacea015@panacea015s-imac.local>
2019-08-16 14:12:54 +0530

    Initial Commit

---
+++ newone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newone
+//
+//  Created by Panacea015 on 16/08/19.
+//  Copyright © 2019 Panacea015. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
