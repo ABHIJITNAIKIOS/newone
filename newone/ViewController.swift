5bc092b4bd9971854e1b8b67f46f657ae3482e14
Abhijit Naik <abhijit.n@panaceatek.com>
2019-08-16 14:46:38 +0530

    Changed by Abhijit

--- newone/ViewController.swift
+++ newone/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         
         
         print("commiting")
+            print("commited in abhijit")
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
